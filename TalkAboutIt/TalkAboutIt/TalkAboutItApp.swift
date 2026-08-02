904c0f2ce694bd6b730970df7e22a2b603f3d0f8
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:19:51 -0400

    Add TalkAboutIt/TalkAboutIt/TalkAboutItApp.swift

---
+++ TalkAboutIt/TalkAboutIt/TalkAboutItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkAboutItApp.swift
+//  TalkAboutIt
+//
+//  Created by Elliot Williams on 2025-06-20.
+//
+
+import SwiftUI
+
+@main
+struct TalkAboutItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
