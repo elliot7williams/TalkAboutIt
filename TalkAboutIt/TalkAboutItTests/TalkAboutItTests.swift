27bfb5c28979b491e66fd218363ea232ef390c10
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:19:41 -0400

    Add TalkAboutIt/TalkAboutItTests/TalkAboutItTests.swift

---
+++ TalkAboutIt/TalkAboutItTests/TalkAboutItTests.swift
@@ -0,0 +1,17 @@
+//
+//  TalkAboutItTests.swift
+//  TalkAboutItTests
+//
+//  Created by Elliot Williams on 2025-06-20.
+//
+
+import Testing
+@testable import TalkAboutIt
+
+struct TalkAboutItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
